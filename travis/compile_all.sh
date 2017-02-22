990b17ea1d27c5177e2ba2154cd37d4a259b521e
Matthieu Gautier <mgautier@kymeria.fr>
2017-02-22 18:55:37 +0100

    Compile android kiwix-lib with travis.

--- travis/compile_all.sh
+++ travis/compile_all.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-if [ "${STATIC_BUILD}" = "true" ]; then
-    OPTION="${BUILD_TARGET}_static"
-else
-    OPTION="${BUILD_TARGET}_dyn"
-fi
-
-./kiwix-build.py --build-target=${OPTION}
+./kiwix-build.py $BUILD_OPTION
